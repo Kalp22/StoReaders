bc97c018d33a6a2de4d6e8d3db54c062b530cb86
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2024-06-11 03:07:44 +0530

    chore: Update npm dependencies and refactor CSS styles

--- frontend/src/app/story/[id]/read/[chapterName]/page.jsx
+++ frontend/src/app/story/[id]/read/[chapterName]/page.jsx
@@ -7,6 +7,7 @@ import { Quicksand } from "next/font/google";
 
 import Comments from "@/components/comments/comments";
 import SpinnerLoad from "@/components/loading/spinnerLoad";
+import DarkLight from "@/components/ui/darklight/page";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -25,7 +26,7 @@ const quicksand = Quicksand({
   subsets: ["latin"],
 });
 
-export default function chapterRead({ params: { id, chapterName } }) {
+export default function ChapterRead({ params: { id, chapterName } }) {
   const router = useRouter();
 
   const storyName = decodeURIComponent(id);
@@ -39,6 +40,18 @@ export default function chapterRead({ params: { id, chapterName } }) {
     chapterNumber: "",
     commentId: [],
   });
+
+  const getInitialFontSize = () => {
+    if (typeof window !== "undefined") {
+      const width = window.innerWidth;
+      if (width < 600) return 1.0;
+      if (width < 800) return 1.2;
+      return 1.4;
+    }
+    return 1.4; // Default value if window is undefined
+  };
+
+  const [fontSize, setFontSize] = useState(getInitialFontSize()); // [12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
   const [theme, setTheme] = useState(true);
   const [loading, setLoading] = useState(true);
 
@@ -92,7 +105,7 @@ export default function chapterRead({ params: { id, chapterName } }) {
             }
           );
           const data = await res.json();
-
+          console.log(fontSize);
           if (data.chapter == null) {
             router.push(`/story/${id}`);
           }
@@ -107,43 +120,75 @@ export default function chapterRead({ params: { id, chapterName } }) {
       }
     };
     getChapter();
-  }, [chapter]);
+  }, [chapter_name, storyName, user?.id]); // Only run when chapter_name, storyName, or user.id changes
+
+  const handleFontSize = (size) => () => {
+    if (size === 1 && fontSize > 0.8) {
+      setFontSize(fontSize - 0.2);
+    } else if (size === 2) {
+      setFontSize(getInitialFontSize());
+    } else if (size === 3 && fontSize < 3) {
+      setFontSize(fontSize + 0.2);
+    }
+  };
 
   return (
-    <div className={styles.read_container}>
-      {loading ? (
-        <div className={styles.loading}>
-          <SpinnerLoad />
-          <FaBook size={80} className={styles.book} />
-        </div>
-      ) : (
-        <>
-          <div className={styles.read_wrapper}>
-            <p className={`${styles.story_name} ${nunito.className}`}>
-              {chapter.storyName}
-            </p>
-            <div className={styles.chapter_info}>
-              <span>
-                Chapter Number {chapter.chapterNumber && chapter.chapterNumber}{" "}
-                :
-              </span>
-              <span className={quicksand.className}>{chapter_name}</span>
-            </div>
-            <div className={`${styles.content} ${baskervville.className}`}>
-              {chapter.chapterContent &&
-                chapter.chapterContent.split("\n").map((para, i) => {
-                  return (
-                    <p key={i} className={styles.paragraph}>
-                      {para}
-                    </p>
-                  );
-                })}
-            </div>
+    <>
+      <div className={styles.quick_settings}>
+        <DarkLight />
+        <div className={styles.font_size}>
+          <div className={styles.font_size_btn1} onClick={handleFontSize(1)}>
+            -
           </div>
-          <Comments chapterId={chapter._id} commentIds={chapter.commentId} />
-        </>
-      )}
-      <Toaster theme={theme ? "dark" : "light"} position="bottom-left" />
-    </div>
+          <div className={styles.font_size_btn2} onClick={handleFontSize(2)}>
+            A
+          </div>
+          <div className={styles.font_size_btn3} onClick={handleFontSize(3)}>
+            +
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div className={styles.read_container}>
+        {loading ? (
+          <div className={styles.loading}>
+            <SpinnerLoad />
+            <FaBook size={80} className={styles.book} />
+          </div>
+        ) : (
+          <>
+            <div className={styles.read_wrapper}>
+              <p className={`${styles.story_name} ${nunito.className}`}>
+                {chapter.storyName}
+              </p>
+              <div className={styles.chapter_info}>
+                <span>
+                  Chapter Number{" "}
+                  {chapter.chapterNumber && chapter.chapterNumber} :
+                </span>
+                <span className={quicksand.className}>{chapter_name}</span>
+              </div>
+              <div
+                className={`${styles.content} ${baskervville.className}`}
+                style={{
+                  fontSize: `${fontSize}rem`,
+                }}
+              >
+                {chapter.chapterContent &&
+                  chapter.chapterContent.split("\n").map((para, i) => {
+                    return (
+                      <p key={i} className={styles.paragraph}>
+                        {para}
+                      </p>
+                    );
+                  })}
+              </div>
+            </div>
+            <Comments chapterId={chapter._id} commentIds={chapter.commentId} />
+          </>
+        )}
+        <Toaster theme={theme ? "dark" : "light"} position="bottom-left" />
+      </div>
+    </>
   );
 }
