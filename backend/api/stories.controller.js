289a953ee5bc77d360dd383d0d510d00306281ff
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2024-07-22 03:43:51 +0530

    chore: Update email notification template with Storeaders link

--- backend/api/stories.controller.js
+++ backend/api/stories.controller.js
@@ -205,7 +205,7 @@ const notifyUsers = async (storyName, storyDescription) => {
     <div style="background-color: #f5f5f5; padding: 20px;">
       <div style="background-color: white; padding: 20px; border-radius: 10px;">
         <h1 style="font-family: 'Arial'; color: #333;">Get Ready for an Adventure! 📚</h1>
-        <p style="font-family: 'Arial'; color: #333;">Exciting news! We've just added a brand new story to our collection, and we think you're going to love it!</p>
+        <p style="font-family: 'Arial'; color: #333;">Exciting news! I've just added a brand new story to our collection, and we think you're going to love it!</p>
         <p style="font-family: 'Arial'; color: #333;">Titled "<strong>${storyName}</strong>",</p>
         <p style="font-family: 'Arial'; color: #333;">${storyDescription.slice(
           0,
@@ -213,6 +213,7 @@ const notifyUsers = async (storyName, storyDescription) => {
         )}...</p>
         <p style="font-family: 'Arial'; color: #333;">Ready to start reading? Click <a href="https://storeaders.vercel.app/story/${storyRoute}">here</a> to dive into the adventure right away!</p>
         <p style="font-family: 'Arial'; color: #333;">Don't miss out on this exciting new addition. Happy reading!</p>
+        <p style="font-family: 'Arial'; color: #333;"><a href="https://storeaders.vercel.app">Storeaders</a></p>
       </div>
     </div>
   `;
